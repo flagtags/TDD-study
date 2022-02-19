96f1940a240f7f2c90c1095e92aa25ca84c73ba5
kkirico <48055710+kkirico@users.noreply.github.com>
2022-02-19 14:51:51 +0900

    Header 테스트 수정

--- packages/nemonemologic/src/components/Header/index.test.tsx
+++ packages/nemonemologic/src/components/Header/index.test.tsx
@@ -6,5 +6,5 @@ test('헤더에 타이틀이 알맞게 렌더링 된다.', () => {
   const exampleString = '네모네모 자동차';
   const { getByText } = render(<Header title={exampleString} />);
 
-  getByText(exampleString)
+  expect(getByText(exampleString)).not.toBe(null)
 });
