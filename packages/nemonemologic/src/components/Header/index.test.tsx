e974b21310f4a7e1e2d01b0a68be8d2267ccb66d
kkirico <48055710+kkirico@users.noreply.github.com>
2022-02-19 12:27:41 +0900

    테스트 변경
    테스트 목적 변경으로 인한

--- packages/nemonemologic/src/components/Header/index.test.tsx
+++ packages/nemonemologic/src/components/Header/index.test.tsx
@@ -4,9 +4,7 @@ import Header from './index';
 
 test('헤더에 타이틀이 알맞게 렌더링 된다.', () => {
   const exampleString = '네모네모 자동차';
-  const { container } = render(<Header title={exampleString} />);
-  const title = container.firstChild?.textContent;
+  const { getByText } = render(<Header title={exampleString} />);
 
-  expect(title).not.toBe('wrong string');
-  expect(title).toBe(exampleString);
+  getByText(exampleString)
 });
